4ed240aab8495a94e1c6d0a96c7036ec90edde60
Win Tun <wintun.hlaing@outlook.com>
2023-10-28 16:50:35 +0630

    pyexcel

--- acy-python-excel/testing.py
+++ acy-python-excel/testing.py
@@ -1,1 +1,19 @@
-print(">>>>print hello")
+# print("Hello World")
+# name = "Win Tun"
+# print("Hello",name)
+
+# for i in range(10):
+#     print(i)
+
+# for char in "Python":
+#     print(char)
+
+from openpyxl import load_workbook
+
+
+wb = load_workbook("Books1.xlsx")
+ws=wb.active
+print(ws["A1"])
+print(ws["B2"].value)
+ws["B2"].value="KauThaung"
+ws.save["Books1.xlsx"]
